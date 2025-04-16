3c31005e1b1920515aec73dc87794ce585db1afa
Jay Karasev <jaykarasev@gmail.com>
2025-04-16 01:17:25 -0500

    Fix: use REACT_APP_BASE_URL for production API calls

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001"; // Update if different
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 const usersApi = {
   /** Fetch user profile by ID */
